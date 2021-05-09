edbe9e7b585e03d8b7ab0b587bb3900a26f0733c
Brijesh <brijeshmovaliya34@gmail.com>
2021-05-09 16:23:22 +0530

    Login Register Contact modify

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -11,13 +11,13 @@ const layout = {
 };
 
 const validateMessages = {
-  required: '${label} is required!',
+  required: 'Name is required!',
   types: {
-    email: '${label} is not a valid email!',
-    number: '${label} is not a valid number!',
+    email: 'email is not a valid email!',
+    number: 'number is not a valid number!',
   },
   number: {
-    range: '${label} must be between ${min} and ${max}',
+    range: 'number must be of 10 digit ',
   },
 };
 
@@ -35,22 +35,21 @@ const Contact = () => {
           help you.</b> 
         </p>
       </section>
+        <div className="containerForm">
       <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
         <Form.Item
             name={['user', 'name']}
-            label="Name"
-
             rules={[
               {
                 required: true,
               },
             ]}
         >
-          <Input placeholder=" your Name"/>
+            <Input placeholder=" your Name"/>
         </Form.Item>
+          <br/>
         <Form.Item
             name={['user', 'email']}
-            label="Email"
             rules={[
               {
                 type: 'email',
@@ -58,20 +57,20 @@ const Contact = () => {
             ]}
         >
           <Input placeholder="Your Email "/>
-        </Form.Item>
-        <Form.Item name={['user', 'Phone']} label="Phone">
+        </Form.Item><br/>
+        <Form.Item name={['user', 'Phone']} >
           <Input placeholder="Phone"/>
-        </Form.Item>
-        <Form.Item name={['user', 'your Message']} label="your Message">
+        </Form.Item><br/>
+        <Form.Item name={['user', 'your Message']} >
           <Input.TextArea placeholder="your Message"/>
-        </Form.Item>
+        </Form.Item><br/>
         <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
           <Button type="primary" htmlType="submit">
             Submit
           </Button>
         </Form.Item>
       </Form>
-      
+        </div>
     </div>
   );
 };
