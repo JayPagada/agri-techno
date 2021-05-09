fc0b8c9c7f89963319cecd5e8885d7cdc344e476
JayPagada <jaypagada11@gmail.com>
2021-05-09 22:06:19 +0530

    authentication add and feedback auth added

---
+++ src/Redux/actionsTypes.jsx
@@ -0,0 +1,7 @@
+export const AUTH_START = "AUTH_START"
+export const AUTH_SUCCESS = "AUTH_SUCCESS"
+export const AUTH_FAIL = "AUTH_FAIL"
+export const AUTH_LOGOUT = "AUTH_LOGOUT"
+
+export const FEEDBACK_SUCCESS = "FEEDBACK_SUCCESS"
+export const FEEDBACK_FAIL = "FEEDBACK_FAIL"
